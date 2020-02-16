38e9d33055fe4ef8e876bc7bdc4be3c6ad898d46
Monutor <sporte2222@yandex.ru>
2020-02-16 19:09:01 +0300

    добавлена секция Your Followers - слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,12 @@ $(function () {
 
    var mixer = mixitup('.release-products__items');
 
+   
+
+   $('.follow__items').slick({
+    prevArrow: '<button class="prev slick-arrow"><img src="images/slider/chevron-left.png" alt=""></button>',
+    nextArrow: '<button class="next slick-arrow"><img src="images/slider/chevron-right.png" alt=""></button>',
+    slidesToShow: 3,
+    variableWidth: true,
+   });
 });
