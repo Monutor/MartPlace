7d5a726f3bc09556380a615b84b178fa59a36e0d
Monutor <sporte2222@yandex.ru>
2020-02-20 13:05:22 +0300

    Добавлены секции: "clients" "news" "support" "banner" "footer". Исправлены разные ошибки. Добавлено выпадающие меню при клике на "3 кружка" у фильтра

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,8 @@ $(function () {
     rating: 4.5,
     starWidth: "14px",
     readOnly: true,
-    normalFill: "#fff"
+    normalFill: "#ffc000",
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>'
   });
 
   $('.box-slider').slick({
@@ -23,4 +24,8 @@ $(function () {
     slidesToShow: 3,
     variableWidth: true,
    });
+
+   $('.button').click(function() {
+    $('.filter__box').fadeToggle();
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('script', function(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/mixitup/dist/mixitup.js'
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/@fortawesome/fontawesome-free/js/all.min.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(iglify())
@@ -36,6 +37,7 @@ gulp.task('style', function(){
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
+    'node_modules/@fortawesome/fontawesome-free/css/all.min.css'
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
